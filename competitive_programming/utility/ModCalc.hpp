8bb091f98b33c4428f7c47c1ba9c9ad1ff0e5d97
sitRyo <al15030@shibaura-it.ac.jp>
2019-05-29 22:21:43 +0900

    remake modcalc

--- competitive_programming/utility/ModCalc.hpp
+++ competitive_programming/utility/ModCalc.hpp
@@ -3,7 +3,9 @@
 
 #include <iostream>
 
-class ModCalc {
+// MOD付き計算だけでもできるように計算は別クラスで
+class ModCalcBase 
+{
   using mod_type = long long;
 
   mod_type M = 1000000007;
@@ -37,6 +39,71 @@ public:
     lval *= head % M;
     lval %= M;
     mul(lval, body...);
+  }
+};
+
+// こっちのクラスは値を持つ。operatorを実装しているのでいろいろできる。
+template <class T>
+class ModCalc : public ModCalcBase 
+{
+  using value_type = T;
+  using reference = value_type&;
+public:
+  value_type value;
+  
+  ModCalc(value_type value = 0)
+    : value(value) {}
+
+  // 値の変更はメソッドを使って欲しい。。。
+  value_type
+  operator ()() const
+  {
+    return value;
+  }
+
+  value_type
+  operator +(ModCalc& rhs) const noexcept
+  {
+    value_type tmp = value;
+    add(tmp, rhs.value);
+    return tmp;
+  }
+
+  value_type
+  operator -(ModCalc& rhs) const noexcept
+  {
+    value_type tmp = value;
+    add(tmp, -rhs.value);
+    return tmp;
+  }
+
+  value_type 
+  operator *(ModCalc& rhs)
+  {
+    value_type tmp = value;
+    mul(tmp, rhs.value);
+    return tmp;
+  }
+
+  ModCalc&
+  operator +=(ModCalc& rhs) 
+  {
+    add(value, rhs.value);
+    return *this;
+  }
+
+  ModCalc&
+  operator -=(ModCalc& rhs)
+  {
+    add(value, -rhs.value);
+    return *this;
+  }
+
+  ModCalc&
+  operator *=(ModCalc& rhs)
+  {
+    mul(value, rhs.value);
+    return *this;
   }
 };
 #endif
--- competitive_programming/utility/modint_test.cpp
+++ competitive_programming/utility/modint_test.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
-class ModCalc {
+
+// MOD付き計算だけでもできるように計算は別クラスで
+class ModCalcBase 
+{
   using mod_type = long long;
 
   mod_type M = 1000000007;
@@ -37,19 +40,80 @@ public:
   }
 };
 
+// こっちのクラスは値を持つ。operatorを実装しているのでいろいろできる。
+template <class T>
+class ModCalc : public ModCalcBase 
+{
+  using value_type = T;
+  using reference = value_type&;
+public:
+  value_type value;
+  
+  ModCalc(value_type value = 0)
+    : value(value) {}
+
+  // 値の変更はメソッドを使って欲しい。。。
+  value_type
+  operator ()() const
+  {
+    return value;
+  }
+
+  value_type
+  operator +(ModCalc& rhs) const noexcept
+  {
+    value_type tmp = value;
+    add(tmp, rhs.value);
+    return tmp;
+  }
+
+  value_type
+  operator -(ModCalc& rhs) const noexcept
+  {
+    value_type tmp = value;
+    add(tmp, -rhs.value);
+    return tmp;
+  }
+
+  value_type 
+  operator *(ModCalc& rhs)
+  {
+    value_type tmp = value;
+    mul(tmp, rhs.value);
+    return tmp;
+  }
+
+  ModCalc&
+  operator +=(ModCalc& rhs) 
+  {
+    add(value, rhs.value);
+    return *this;
+  }
+
+  ModCalc&
+  operator -=(ModCalc& rhs)
+  {
+    add(value, -rhs.value);
+    return *this;
+  }
+
+  ModCalc&
+  operator *=(ModCalc& rhs)
+  {
+    mul(value, rhs.value);
+    return *this;
+  }
+};
+
 int main() {
   long long a,b,c;
-  a = 10e9, b = 8, c = 1;
+  a = 10, b = 8, c = 1;
 
-  ModCalc m;
-  m.add(a,b,c);
-
-  a = 23487; b = 141324895; c = 1495837;
-  m.mul(a, b, c);
-
-  if (a > 10e9 + 8) {
-    return 0;
-  }
-  
-  std::cout << a << "\n";
+  ModCalc<long long> m1(a), m2(b), m3(c);
+  m1 *= m2;
+  std::cout << m1() << " " << m2() << "\n";
+  m1 -= m2;
+  std::cout << m1() << " " << m2() << "\n";
+  m1 += m3;
+  std::cout << m1() << " " << m2() << "\n";
 }
