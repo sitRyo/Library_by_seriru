4fa3703a6995f748759d435bd0c150ea4bb4d3b9
sitRyo <al15030@shibaura-it.ac.jp>
2019-09-06 22:37:42 +0900

    revise

--- competitive_programming/data_structure/BinaryIndexedTree.cpp
+++ competitive_programming/data_structure/BinaryIndexedTree.cpp
@@ -3,9 +3,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define LEN 10000010
-int N;
+
+namespace indexed1 {
+constexpr int LEN = 10000010; // for c++11
+int N = LEN;
 int bit[LEN];
+
+// 1-indexedなBIT
 
 // 負の数は2の補数
 // a[index]をvalueだけ追加
@@ -23,6 +27,39 @@ ll get(ll index) {
 // 1-indexed, a[x] ~ a[y]までの区間和を取る。
 ll sum(int x, int y) {
   return get(y) - get(x);
+}
+
+
+
+}
+
+namespace indexed0 {
+
+constexpr int LEN = 10000010; // for c++11
+int N = LEN;
+int bit[LEN];
+
+// 0-indexedなBIT
+
+// 負の数は2の補数
+// a[index]をvalueだけ追加
+void add(ll index, ll value) {
+  for (int i = index+1; i <= N; i += i & (-i)) bit[i] += value;
+}
+
+// 0-indexed, a[1] ~ a[index] の総和
+ll get(ll index) {
+  ll ret = 0;
+  index += 1;
+  for (int i = index; i > 0; i -= i & (-i)) ret += bit[i];
+  return ret;
+}
+
+// 0-indexed, a[x] ~ a[y]までの区間和を取る。
+ll sum(int x, int y) {
+  return get(y) - get(x);
+}
+
 }
 
 int main() {}
